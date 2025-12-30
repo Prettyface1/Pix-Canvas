f0cfad9cc37431708f7419b5964843fa43001e4d
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:06:58 +0100

    feat: add accessibility tags to ColorPicker

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './ColorPicker.css';
 
-export const ColorPicker: React.FC = () => <div>ColorPicker</div>;
+export const ColorPicker: React.FC = () => <div aria-label="ColorPicker">ColorPicker</div>;
