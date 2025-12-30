47ad0de210ab78718696e36ee9dc786e3a5bc03b
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:31:02 +0100

    feat: add Tooltip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Tooltip.css';
 
-export const Tooltip: React.FC = (props: any) => <div aria-label="Tooltip">Tooltip</div>;
+export const Tooltip: React.FC<TooltipProps> = () => <div>Tooltip</div>;
