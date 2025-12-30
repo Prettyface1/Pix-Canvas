323c3d91c1dece1db03652e994d40719d6b357e5
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:05:38 +0100

    feat: add accessibility tags to Toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Toolbar.css';
 
-export const Toolbar: React.FC = () => <div>Toolbar</div>;
+export const Toolbar: React.FC = () => <div aria-label="Toolbar">Toolbar</div>;
