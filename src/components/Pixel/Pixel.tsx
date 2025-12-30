dd28cd1f5dbee69c4958aea2f927fd73e3bea1a5
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:05:17 +0100

    feat: add accessibility tags to Pixel

--- src/components/Pixel/Pixel.tsx
+++ src/components/Pixel/Pixel.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Pixel.css';
 
-export const Pixel: React.FC = () => <div>Pixel</div>;
+export const Pixel: React.FC = () => <div aria-label="Pixel">Pixel</div>;
