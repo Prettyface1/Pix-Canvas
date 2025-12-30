96021d9860806d31fb13d161271b48fd9ccb37e5
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:54:16 +0100

    feat(grid): implement component logic

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,4 +1,3 @@
 import React from 'react';
 import './Grid.css';
-
-export const Grid: React.FC = (props: any) => <div aria-label="Grid">Grid</div>;
+export const Grid = () => <div className="grid">Grid content</div>;
