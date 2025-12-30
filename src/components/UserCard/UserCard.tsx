2fb9c519f5bde5d555324bb52e0171b1060c1447
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:27:19 +0100

    feat: add UserCard component

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './UserCard.css';
 
-export const UserCard: React.FC = (props: any) => <div aria-label="UserCard">UserCard</div>;
+export const UserCard: React.FC<UserCardProps> = () => <div>UserCard</div>;
