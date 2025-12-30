79991b098873682bd5a0c9a2d375a30f534fa2c9
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:54:42 +0100

    feat(statspanel): add props handling

--- src/components/StatsPanel/StatsPanel.tsx
+++ src/components/StatsPanel/StatsPanel.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './StatsPanel.css';
 
-export const StatsPanel = () => <div className="statspanel">StatsPanel</div>;
+export const StatsPanel = (props: any) => <div className="statspanel">StatsPanel</div>;
