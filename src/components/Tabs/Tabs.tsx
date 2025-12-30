5196adbcebfb4e4974f634abc0e756512eed4c06
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:35:47 +0100

    feat: add Tabs component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Tabs.css';
+
+export const Tabs: React.FC<TabsProps> = () => <div>Tabs</div>;
---
+++ src/hooks/__tests__/useInterval.test.ts
@@ -0,0 +1,1 @@
+describe('useInterval', () => { it('works', () => {}) })
