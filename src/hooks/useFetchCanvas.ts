9e4a8e51706754e8c557abe4044ae0c7c3645340
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:35:31 +0100

    test(hooks): test useToggle reactivity

--- src/hooks/useFetchCanvas.ts
+++ src/hooks/useFetchCanvas.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // useFetchCanvas Types
 export const useFetchCanvas = () => {
   const [s, setS] = useState();
