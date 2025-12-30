3186bad04ea84a8c52ec3701e963ae1f571a8164
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:08:59 +0100

    feat: add accessibility tags to Leaderboard

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Leaderboard.css';
 
-export const Leaderboard: React.FC = () => <div>Leaderboard</div>;
+export const Leaderboard: React.FC = () => <div aria-label="Leaderboard">Leaderboard</div>;
