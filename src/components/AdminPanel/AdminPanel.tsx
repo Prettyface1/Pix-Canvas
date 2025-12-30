2be9189224eaf1a0a483b651b01346979b9384ac
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:02:00 +0100

    feat(adminpanel): implement component logic

--- src/components/AdminPanel/AdminPanel.tsx
+++ src/components/AdminPanel/AdminPanel.tsx
@@ -1,4 +1,3 @@
 import React from 'react';
 import './AdminPanel.css';
-
-export const AdminPanel: React.FC = (props: any) => <div aria-label="AdminPanel">AdminPanel</div>;
+export const AdminPanel = () => <div className="adminpanel">AdminPanel content</div>;
