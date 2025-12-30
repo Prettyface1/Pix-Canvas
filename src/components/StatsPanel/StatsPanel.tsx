c14a0fa31499258a317257cb35818cca89778124
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:54:42 +0100

    feat(statspanel): add loading state logic

--- src/components/StatsPanel/StatsPanel.tsx
+++ src/components/StatsPanel/StatsPanel.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './StatsPanel.css';
 
-export const StatsPanel = (props: any) => <div className="statspanel">StatsPanel</div>;
+export const StatsPanel = (props: any) => { const loading = false; return ( <div className="statspanel">StatsPanel</div>;)}
