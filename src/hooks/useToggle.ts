778026eb8e802bba79d63f8308a7add06ce6d9dc
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:40:16 +0100

    feat(hooks): add useToggle effect

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // useToggle Types
 export const useToggle = () => {
   const [s, setS] = useState();
