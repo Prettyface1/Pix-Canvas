596f943abda14d1f5177d3ae433fc1dcb11bb58b
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:08:31 +0100

    feat: implement Filter logic

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Filter.css';
 
-export const Filter: React.FC = () => <div aria-label="Filter">Filter</div>;
+export const Filter: React.FC = (props: any) => <div aria-label="Filter">Filter</div>;
