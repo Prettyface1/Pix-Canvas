29af2b98470efdc9db1a698dbb2f201f30e3be60
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:07:19 +0100

    feat(settings): implement component logic

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,4 +1,3 @@
 import React from 'react';
 import './Settings.css';
-
-export const Settings: React.FC = (props: any) => <div aria-label="Settings">Settings</div>;
+export const Settings = () => <div className="settings">Settings content</div>;
