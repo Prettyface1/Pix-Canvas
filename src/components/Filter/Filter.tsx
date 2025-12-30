7e30fd79ece346716affcccda18be7fbaa62d0b4
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:02:43 +0100

    feat(sidebar): init directory

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Filter.css';
 
-export const Filter: React.FC = (props: any) => <div aria-label="Filter">Filter</div>;
+export const Filter: React.FC = (props: any) => { const loading = false; return ( <div aria-label="Filter">Filter</div>;)}
