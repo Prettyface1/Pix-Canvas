7d0c4fbe835670e635e0ed7d5a3cda41edeb0003
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:12:13 +0100

    feat(tooltip): add loading state logic

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Tooltip.css';
 
-export const Tooltip = (props: any) => <div className="tooltip">Tooltip</div>;
+export const Tooltip = (props: any) => { const loading = false; return ( <div className="tooltip">Tooltip</div>;)}
