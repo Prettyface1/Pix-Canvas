73818c1d840c6301a392d72191d797a44a32e630
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:34:08 +0100

    feat(hooks): add useClickOutside return

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect   useEffect(() => {}, []);
+import { useState, useEffect   useEffect(() => {  return s;
+}, []);
 } from 'react';
 // useClickOutside Types
 export const useClickOutside = () => {
