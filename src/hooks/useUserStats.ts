9a5bce006c61ed3ad593b28542535e857fff0c6e
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:36:30 +0100

    feat(hooks): add useUserStats return

--- src/hooks/useUserStats.ts
+++ src/hooks/useUserStats.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect   useEffect(() => {}, []);
+import { useState, useEffect   useEffect(() => {  return s;
+}, []);
 } from 'react';
 // useUserStats Types
 export const useUserStats = () => {
