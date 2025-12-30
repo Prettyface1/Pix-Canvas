95beb23ae682b71f72beccc3db0c337bc664705c
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:19:57 +0100

    feat: add accessibility tags to Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Pagination.css';
 
-export const Pagination: React.FC = () => <div>Pagination</div>;
+export const Pagination: React.FC = () => <div aria-label="Pagination">Pagination</div>;
