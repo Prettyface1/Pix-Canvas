b27795f4d39e6622c2588ec5e1ef82652ff4c72b
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:29:03 +0100

    feat(hooks): add usePixel effect

--- src/hooks/usePixel.ts
+++ src/hooks/usePixel.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // usePixel Types
 export const usePixel = () => {
   const [s, setS] = useState();
