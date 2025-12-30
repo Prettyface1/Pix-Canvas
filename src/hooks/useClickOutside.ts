772124734a08af048b4debc91cec02d7f55bbf87
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:33:55 +0100

    feat(hooks): add useClickOutside state

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,3 +1,5 @@
 import { useState, useEffect } from 'react';
 // useClickOutside Types
-export const useClickOutside = () => {};
+export const useClickOutside = () => {
+  const [s, setS] = useState();
+};
