2dc7430a2674581bb7d5b4d0fde11551451c0034
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:17:59 +0100

    feat: add Toolbar component

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Toolbar.css';
 
-export const Toolbar: React.FC = (props: any) => <div aria-label="Toolbar">Toolbar</div>;
+export const Toolbar: React.FC<ToolbarProps> = () => <div>Toolbar</div>;
