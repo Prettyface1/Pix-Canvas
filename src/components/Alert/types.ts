c9e7920c2b76af33df9e8f0c52956d9288c0639c
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:14:32 +0100

    feat: add variants to Alert types

---
+++ src/components/Alert/types.ts
@@ -0,0 +1,2 @@
+
+export type AlertVariant = "primary" | "secondary";
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Footer.css';
 
-export const Footer: React.FC = (props: any) => <div aria-label="Footer">Footer</div>;
+export const Footer: React.FC<FooterProps> = () => <div>Footer</div>;
