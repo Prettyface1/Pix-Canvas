154e1f54da8225e346d5e406fff45a4314c0efb2
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:23:43 +0100

    feat: add Instructions component

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Instructions.css';
 
-export const Instructions: React.FC = (props: any) => <div aria-label="Instructions">Instructions</div>;
+export const Instructions: React.FC<InstructionsProps> = () => <div>Instructions</div>;
