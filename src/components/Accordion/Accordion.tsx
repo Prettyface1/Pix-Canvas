31d4622d2a429751149dac3faa2e589d5e5684d2
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:22:12 +0100

    feat: implement Accordion logic

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Accordion.css';
 
-export const Accordion: React.FC = () => <div aria-label="Accordion">Accordion</div>;
+export const Accordion: React.FC = (props: any) => <div aria-label="Accordion">Accordion</div>;
