309a4cf8f6187f34bf04614875eb961d25068ccb
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:20:35 +0100

    feat(spinner): add props handling

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Spinner.css';
 
-export const Spinner = () => <div className="spinner">Spinner</div>;
+export const Spinner = (props: any) => <div className="spinner">Spinner</div>;
