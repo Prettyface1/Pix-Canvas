3867d5c20010c21a1c89624b3cf99396bd8800ea
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:07:18 +0100

    feat: add accessibility tags to Instructions

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Instructions.css';
 
-export const Instructions: React.FC = () => <div>Instructions</div>;
+export const Instructions: React.FC = () => <div aria-label="Instructions">Instructions</div>;
