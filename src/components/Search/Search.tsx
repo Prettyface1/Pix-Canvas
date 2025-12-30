51bc8cc5d5aee116cd896664f7af52bc3d2f7aeb
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:08:14 +0100

    feat: add accessibility tags to Search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Search.css';
 
-export const Search: React.FC = () => <div>Search</div>;
+export const Search: React.FC = () => <div aria-label="Search">Search</div>;
