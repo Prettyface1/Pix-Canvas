c2f6e485aaf5499769ecfb2971e0f99bd582410f
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:03:35 +0100

    feat(usercard): add props handling

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './UserCard.css';
 
-export const UserCard = () => <div className="usercard">UserCard</div>;
+export const UserCard = (props: any) => <div className="usercard">UserCard</div>;
