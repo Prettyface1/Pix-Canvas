7ef86e9a8b2d90e50e0467beab328a5176d59536
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:57:52 +0100

    feat(rewards): create core files

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Instructions.css';
 
-export const Instructions = (props: any) => <div className="instructions">Instructions</div>;
+export const Instructions = (props: any) => { const loading = false; return ( <div className="instructions">Instructions</div>;)}
