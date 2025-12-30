2e3660cc17cffabe388cba4e56df169f95ff224c
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:39:41 +0100

    feat(hooks): add usePrevious effect

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // usePrevious Types
 export const usePrevious = () => {
   const [s, setS] = useState();
