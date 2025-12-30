52db1bc211c44d2a4132970acd8b1ca19f8d9e9e
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:53:58 +0100

    feat(transactionlist): add loading state logic

--- src/components/TransactionList/TransactionList.tsx
+++ src/components/TransactionList/TransactionList.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './TransactionList.css';
 
-export const TransactionList = (props: any) => <div className="transactionlist">TransactionList</div>;
+export const TransactionList = (props: any) => { const loading = false; return ( <div className="transactionlist">TransactionList</div>;)}
