09560aeb85ab3ae7e673f6194d15374e71252d10
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:32:41 +0100

    feat(hooks): add useWindowSize effect

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // useWindowSize Types
 export const useWindowSize = () => {
   const [s, setS] = useState();
