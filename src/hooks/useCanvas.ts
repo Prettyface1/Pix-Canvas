70e6be705b881ebc0413f91bfd2fe5da5f12b7b0
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:29:49 +0100

    feat(hooks): add useCanvas return

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect   useEffect(() => {}, []);
+import { useState, useEffect   useEffect(() => {  return s;
+}, []);
 } from 'react';
 // useCanvas Types
 export const useCanvas = () => {
