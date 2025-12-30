f81febd31043e16e2c8dd2feab24d17a3b58be75
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:06:41 +0100

    feat(activityfeed): export from registry

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Settings.css';
 
-export const Settings: React.FC = (props: any) => <div aria-label="Settings">Settings</div>;
+export const Settings: React.FC = (props: any) => { const loading = false; return ( <div aria-label="Settings">Settings</div>;)}
