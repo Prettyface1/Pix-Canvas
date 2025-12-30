b50375c6fd8228430c96bfab330751cd470a6342
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:15:56 +0100

    feat(checkbox): performance optimization

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import './Checkbox.css';
 export const Checkbox = () => <div className="checkbox">Checkbox content</div>;
 export const Checkbox = () => <div className="checkbox">Checkbox</div>;
+// optimized
