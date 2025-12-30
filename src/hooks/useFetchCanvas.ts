ff8d6aa3b86132ef96f23470cfad5a1d9fa785c8
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:35:32 +0100

    feat(hooks): add useFetchCanvas return

--- src/hooks/useFetchCanvas.ts
+++ src/hooks/useFetchCanvas.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect   useEffect(() => {}, []);
+import { useState, useEffect   useEffect(() => {  return s;
+}, []);
 } from 'react';
 // useFetchCanvas Types
 export const useFetchCanvas = () => {
