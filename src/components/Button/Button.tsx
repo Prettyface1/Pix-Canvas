049fb8696b4023dc2a83a4f409106290952e937c
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:09:45 +0100

    feat(feature-14): perform step 1 for granular architecture

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = () => <div className="button">Button</div>;
+export const Button = () => { const loading = false; return ( <div className="button">Button</div>;)}
