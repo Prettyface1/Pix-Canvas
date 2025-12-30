ca8342fea01c76fbec0fdc764874a229f7401544
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:14:34 +0100

    feat: implement Alert logic

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Alert.css';
 
-export const Alert: React.FC = () => <div aria-label="Alert">Alert</div>;
+export const Alert: React.FC = (props: any) => <div aria-label="Alert">Alert</div>;
