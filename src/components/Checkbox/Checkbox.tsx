959c99cd937d98a43e73aa0429bd8c3975cadb56
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:14:56 +0100

    feat(checkbox): define component structure

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import './Checkbox.css';
 export const Checkbox = () => <div className="checkbox">Checkbox content</div>;
+export const Checkbox = () => <div className="checkbox">Checkbox</div>;
