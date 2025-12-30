99b69ddfee0333cbdeac906bf4f08d03ccc71c5b
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:08:41 +0100

    feat(modal): add indexing

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import './Leaderboard.css';
 
 export const Leaderboard: React.FC = (props: any) => { const loading = false; return ( <div aria-label="Leaderboard">Leaderboard</div>;)}
+// optimized
