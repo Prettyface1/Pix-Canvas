c15aef506499b688f30d190bb8aa4eecfe182b1a
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:31:49 +0100

    feat: add Checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Checkbox.css';
 
-export const Checkbox: React.FC = (props: any) => <div aria-label="Checkbox">Checkbox</div>;
+export const Checkbox: React.FC<CheckboxProps> = () => <div>Checkbox</div>;
