cc6a5e0c6e6250789d9c1c592b38d5b1f439a915
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:23:58 +0100

    feat(emptystate): export from registry

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Pagination.css';
 
-export const Pagination = (props: any) => <div className="pagination">Pagination</div>;
+export const Pagination = (props: any) => { const loading = false; return ( <div className="pagination">Pagination</div>;)}
