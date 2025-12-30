192cf00c8c59ba00e68f799f2be69fe0ad735d2c
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:53:18 +0100

    feat(header): add base css styling

--- src/components/WalletModal/WalletModal.tsx
+++ src/components/WalletModal/WalletModal.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './WalletModal.css';
 
-export const WalletModal = (props: any) => <div className="walletmodal">WalletModal</div>;
+export const WalletModal = (props: any) => { const loading = false; return ( <div className="walletmodal">WalletModal</div>;)}
