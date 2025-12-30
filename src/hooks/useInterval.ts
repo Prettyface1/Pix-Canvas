a196e1282370b9a0a5174bf380410bfb33320d5b
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:39:14 +0100

    feat(hooks): add useInterval effect

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // useInterval Types
 export const useInterval = () => {
   const [s, setS] = useState();
