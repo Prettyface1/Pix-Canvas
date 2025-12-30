8e969c74aafd04fc0d16ea886457410633bd5fa7
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:18:36 +0100

    feat(badge): add indexing

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import './Badge.css';
 
-export const Badge = (props: any) => <div className="badge">Badge</div>;
-// optimized
+export const Badge = (props: any) => { const loading = false; return ( <div className="badge">Badge</div>;
+// optimized)}
