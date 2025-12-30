4a28374f2a1bdbe91953a99f55314013730a74d1
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:35:18 +0100

    feat: add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Pagination.css';
+
+export const Pagination: React.FC<PaginationProps> = () => <div>Pagination</div>;
---
+++ src/hooks/__tests__/useEvents.test.ts
@@ -0,0 +1,1 @@
+describe('useEvents', () => { it('works', () => {}) })
