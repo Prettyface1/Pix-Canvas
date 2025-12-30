ff64786905194e35ea82c1850b700ecd70b34c10
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 02:30:31 +0100

    feat(hooks): add useWallet effect

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect   useEffect(() => {}, []);
+} from 'react';
 // useWallet Types
 export const useWallet = () => {
   const [s, setS] = useState();
