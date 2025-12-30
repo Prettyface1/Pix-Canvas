f074740cca8699169e4b3cf5a5a21d347824e738
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:11:16 +0100

    feat: implement Tooltip logic

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Tooltip.css';
 
-export const Tooltip: React.FC = () => <div aria-label="Tooltip">Tooltip</div>;
+export const Tooltip: React.FC = (props: any) => <div aria-label="Tooltip">Tooltip</div>;
