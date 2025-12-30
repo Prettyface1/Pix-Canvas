ab942dabdbb71a3514a82d38331ee1a5ad6da7ab
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:57:50 +0100

    feat(instructions): add props handling

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Instructions.css';
 
-export const Instructions = () => <div className="instructions">Instructions</div>;
+export const Instructions = (props: any) => <div className="instructions">Instructions</div>;
