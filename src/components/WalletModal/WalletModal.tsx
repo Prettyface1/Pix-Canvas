d0f4e9edaa85e96f6861b5dbdb610cbb7a5ed4fe
Prettyface1 <boluwatife.dammy@example.com>
2025-12-30 01:05:57 +0100

    feat: implement WalletModal logic

--- src/components/WalletModal/WalletModal.tsx
+++ src/components/WalletModal/WalletModal.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import './WalletModal.css';
 
-export const WalletModal: React.FC = () => <div aria-label="WalletModal">WalletModal</div>;
+export const WalletModal: React.FC = (props: any) => <div aria-label="WalletModal">WalletModal</div>;
